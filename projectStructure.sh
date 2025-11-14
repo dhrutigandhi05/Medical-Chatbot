5354e29d3647d6558b6a8efe301734fe86852d61
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-11-13 21:07:08 -0500

    project structure added

---
+++ projectStructure.sh
@@ -0,0 +1,18 @@
+mkdir -p src
+mkdir -p research
+mkdir -p data
+mkdir -p html
+mkdir -p styles
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch styles/styles.css
+touch html/index.html
+
+echo "Project structure created"
